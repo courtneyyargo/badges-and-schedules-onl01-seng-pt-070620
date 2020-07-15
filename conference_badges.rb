d54875cd16f8553feb0645ff8ecd12d245b9b6eb
Courtney Yargo <courtneyyargo@gmail.com>
2020-07-15 00:54:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def badge_maker(name)
+ "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each do |name|
+    badges << "Hello, my name is #{name}."
+  end
+  badges
+end
+
+def assign_rooms(attendees)
+  rooms = []
+  attendees.each_with_index do |name, index|
+  rooms << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  end
+  rooms
+end
+
+def printer(batch_badge_creator, assign_rooms)
+  batch_badge_creator(name).each
